00742399cd2ca797311dd6c391016993b3a1e1f3
mac-zhubo <mac@work.com>
2019-05-08 13:54:21 +0800

    修改数据库连接用户名

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const users = require('./router/api/user')
 
-mongoose.connect('mongodb://localhost:27017/admin',{useNewUrlParser:true})
+mongoose.connect('mongodb://zhubo:111111@localhost:27017/admin',{useNewUrlParser:true})
     .then(() => console.log('cuccess'))
     .catch((err) => console.log(err))
 
@@ -38,3 +38,20 @@ app.listen(port, () => {
 //
 // processManagement:
 //     fork = true
+
+// docker run -p 27017:27017 -v <LocalDirectoryPath>:/data/db --name docker_mongodb -d mongo
+//
+// docker run -p 27017:27017 -v /data/mongodb0:/data/db --name mongodb-server0  -d docker_mongo --auth
+//
+// docker run --name mongo -p 27017:27017 -v /home/data/db:/data/db -d mongo
+//
+// sudo docker run -it mongo mongo --host 123.56.11.141 --port 27017
+//
+// db.createUser({ user: 'zhubo', pwd: '111111', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
+
+//
+// docker run -it --rm --link mongodb_docker:mongo mongo mongo --host mongo -u zhubo -p 111111 --authenticationDatabase zhubo
+//
+// docker run -it --rm --link mongodb_docker:mongo mongo mongo --host mongo -u zhubo -p 111111 --authenticationDatabase admin
+//
+// db.grantRolesToUser("zhubo", [ { role:"dbOwner", db:"admin"} ]) ;
